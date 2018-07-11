ed7e0f27ac55e50e0fadb526ddf400736821e6ac
Ryan Lee <ryan.j.lee99@gmail.com>
2018-07-11 00:07:20 -0700

    Implement template and view for guessthatsong home view

---
+++ guessthatsong/home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.base import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        """
+        Load context data for home page
+        """
+        context = super().get_context_data(**kwargs)
+        context['song_html'] = 'workfromhome'
+        return context
+    
+    def post(self, request):
+        """
+        Define post function of web page
+        """
+        return dj_short.redirect("/home/")
+
