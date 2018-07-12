a7eddb1dc67c274f537f8027a384d44295c6cc05
Ryan Lee <ryan.j.lee99@gmail.com>
2018-07-12 00:06:13 -0700

    WIP: template does not render context data

--- guessthatsong/home/views.py
+++ guessthatsong/home/views.py
@@ -2,15 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from django.views.generic.base import TemplateView
-import urllib.request as urllib2
+import urllib.request
+
+# Variables
+song_url_base = "http://www.azlyrics.com/lyrics/#FILLIN#.html"
 
 # Helper functions
 
 def get_song_html(artist, song):
-    response = urllib2.urlopen('http://python.org/')
-    html = response.read()
-    print(html)
+    song_url = song_url_base.replace("#FILLIN#", artist + "/" + song)
+    print("Getting html from", song_url)
+    response = urllib.request.urlopen(song_url)
+    song_raw_html = response.read()
+    song_html = get_song_from_raw_html(song_raw_html)
+    return song_html
 
+def get_song_from_raw_html(raw_html):
+    song_start = raw_html.index("<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->".encode()) + 133
+    song_end = raw_html.index("<!-- MxM banner -->".encode()) - 14
+    song_html = raw_html[song_start:song_end]
+    return song_html
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -19,7 +30,6 @@ class HomeView(TemplateView):
         Load context data for home page
         """
         context = super().get_context_data(**kwargs)
-        context['song_html'] = 'workfromhome'
         return context
     
     def post(self, request):
@@ -28,5 +38,6 @@ class HomeView(TemplateView):
         """
         song = request.POST["song"]
         artist = request.POST["artist"]
-        get_song_html(artist, song)
-        return render(request, "home.html")
+        song_html = get_song_html(artist, song)
+        context = {"song_html": song_html}
+        return render(request, "home.html", context)
